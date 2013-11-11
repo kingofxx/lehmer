8efca1a029c3df0f180954684a2f0904874b8f75
Ezekiel Chopper <lcarsos@lcarsos.com>
2013-11-11 10:27:33 -0700

    Initializing the lehmer generator correctly

--- lehmer.d
+++ lehmer.d
@@ -1,4 +1,7 @@
+module lehmer;
+
 class Random {
+    private:
     // Any full-period, modulus-compatible number to generate the sequence of
     // random numbers
     long multiplier;
@@ -9,34 +12,41 @@ class Random {
     // This "state" is the seed.
     // not static to allow multiple instances of this lehmer generator, just
     // because
-    long state;
+    long mstate;
 
-    //TODO D constructor allows for giving seed
-    this(long s) {
-        this.state = s;
+    public:
+    this(long s = 1) {
+        mstate = s;
+        modulus = 2147483647;
+        multiplier = 48271;
     }
 
-    //TODO Fancy D style accessor for seed
+    long state() const @property {
+        return mstate;
+    }
+
+    void state(long s) @property {
+        mstate = s;
+    }
 
     double random() {
         long quotient = modulus / multiplier;
-        long remainder = modulus % remainder;
-        long t = multiplier * (state % quotient) - remainder * (state / quotient);
+        long remainder = modulus % multiplier;
+        long t = multiplier * (mstate % quotient) - remainder * (mstate / quotient);
         if (t > 0)
-            state = t;
+            mstate = t;
         else
-            state = t + M;
+            mstate = t + modulus;
         return cast(double)state / cast(double)modulus;
     }
-
-    //TODO Unit test
-    // From the book:
-    // If multiplier is set to 48271, and initial value of state is 1, then
-    // after 10,000 calls to random() state should be 399,268,537
 }
 
+// From the book:
+// If multiplier is set to 48271, and initial value of state is 1, then
+// after 10,000 calls to random() state should be 399,268,537
 unittest {
     Random rand = new Random(1);
+    assert(rand.state == 1);
 
     for(int i = 0; i < 10000; i++) {
         rand.random();
